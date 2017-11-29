9412f3ca6a7299ef6f0c94316951004ed8b90dce
imzack <zengyangzhao@outlook.com>
2017-11-29 21:23:34 +0800

    add new file

---
+++ 2017-11-29.py
@@ -0,0 +1,23 @@
+"""
+字符串的操作
+"""
+
+name = 'xiaoming'
+address = "china"
+age = 18
+money = 100.1021
+
+print(name,address)
+
+print(name+" "+address)
+
+
+#作业 修改下面语句 能够打印出来  my name is xiaoming, i live in china. i am 18 years old, i hava 100.1021 money
+print("my name is %s, i live in %s. i am %d years old, i hava %d money "%(name,address,age,money))
+
+
+print(name * 2)
+
+
+
+

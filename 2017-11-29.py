8601b683b75b69dbc3f4836e42b6eef2cb92fc7f
Zack <zack@leanpro.cn>
2017-11-29 23:49:24 +0800

    and

--- 2017-11-29.py
+++ 2017-11-29.py
@@ -15,6 +15,7 @@ print(name+" "+address)
 #作业 修改下面语句 能够打印出来  my name is xiaoming, i live in china. i am 18 years old, i hava 100.1021 money
 print("my name is %s, i live in %s. i am %d years old, i hava %d money "%(name,address,age,money))
 
+print("my name is %s, i live in %s. i am %d years old, i hava %f money "%(name,address,age,money))
 
 print(name * 2)
 
---
+++ 2017-11-30.py
@@ -0,0 +1,6 @@
+#! /Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+a = 0
+b = 10 
+print ("a and b ",a and b)
+

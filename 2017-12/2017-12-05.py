c417333f6a271868cb0810479015e33d0fe648c3
imzack <zengyangzhao@outlook.com>
2017-12-05 09:37:03 +0800

    元组

---
+++ 2017-12/2017-12-05.py
@@ -0,0 +1,46 @@
+'''
+python3 元组 tuple
+'''
+
+tup3 = "a", "b", "c", "d"
+print(tup3)
+tup1 = ()
+print(tup1)
+
+print(len(tup3))
+
+# tup3[0] = "1"
+tup1 = ('Google', 'Runoob', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5, 6, 7 )
+
+print ("tup1[0]: ", tup1[0])
+print ("tup2[1:5]: ", tup2[1:5])
+
+
+tup1 = (12, 34.56);
+tup2 = ('abc', 'xyz')
+tup3 = tup1 + tup2;
+print (tup3)
+
+
+del tup1
+
+list1= ['Google', 'Taobao', 'Runoob', 'Baidu'] 
+
+tup1 = tuple(list1)
+print(tup1)
+
+tup4=tuple('hello world')
+print(tup4)
+
+
+'''
+作业
+'''
+
+#元组运算符
+
+
+
+
+#思考. 元组和列表之间相同之处和不同之处

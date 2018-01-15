990bb04a39e7b214f234227b62aebcb98b589585
imzack <zengyangzhao@outlook.com>
2018-01-15 08:38:55 +0800

    2018-01-15_selenium-python第一个简单的ui自动化入门

---
+++ 2018-01/2018-01-02.py
@@ -0,0 +1,14 @@
+"""
+python3 递归函数
+"""
+
+def f (n):
+    if n == 1:
+        return 1
+    return n * f(n-1)
+
+print(f(1))
+print(f(3))
+print(f(5))
+
+print(f(1000))
---
+++ 2018-01/2018-01-15.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+driver =  webdriver.Chrome(executable_path="./chromedriver.exe")
+
+driver.get("http://www.baidu.com")
+
+searchInput = driver.find_element_by_id('kw')
+
+searchInput.send_keys("helloworld")
+
+driver.quit()
